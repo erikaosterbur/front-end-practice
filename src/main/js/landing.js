5a37a122eb658840c7e3f2f48058296455e46742
erikaosterbur <erikaosterbur@gmail.com>
2022-03-29 10:32:58 -0500

    added landing.js to fade in images and centered forms on login and register pages

---
+++ src/main/js/landing.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.container').each( function(i){
+
+            let bottom_of_object = $(this).position().top + $(this).outerHeight();
+            let bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it in */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1500);
+            }
+        });
+    });
+});
