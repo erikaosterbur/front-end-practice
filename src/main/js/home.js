005092716c2f945e9872cfa1e7b3ff4d85dfbf1e
erikaosterbur <erikaosterbur@gmail.com>
2022-03-30 11:52:06 -0500

    added images to pet cards

--- src/main/js/home.js
+++ src/main/js/home.js
@@ -4,25 +4,33 @@ $(document).ready(function() {
             "name": "Ginny",
             "type": "Dog",
             "breed": "Border Collie",
-            "birthday": "08/26/2015"
+            "birthday": "08/26/2015",
+            "img": "../images/dog.jpg",
+            "alt": "image of Ginny"
         },
         pet2: {
             "name": "Lily",
             "type": "Cat",
             "breed": "Tabby",
-            "birthday": "06/15/2013"
+            "birthday": "06/15/2013",
+            "img": "../images/cat.jpg",
+            "alt": "image of Lily"
         },
         pet3: {
             "name": "Piggy",
             "type": "Guinea Pig",
             "breed": "Teddy Guinea Pig",
-            "birthday": "02/09/2019"
+            "birthday": "02/09/2019",
+            "img": "../images/piggy.jpg",
+            "alt": "image of Piggy"
         },
         pet4: {
             "name": "Julio",
             "type": "Bird",
             "breed": "Parakeet",
-            "birthday": "04/03/2020"
+            "birthday": "04/03/2020",
+            "img": "../images/bird.jpg",
+            "alt": "image of Julio"
         }
     }
 
@@ -31,14 +39,17 @@ $(document).ready(function() {
         let type = pet[i].type;
         let breed = pet[i].breed;
         let birthday = pet[i].birthday;
+        let img = pet[i].img;
+        let alt = pet[i].alt;
 
         $('#petDiv').append(
             `<div class="card m-5" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">`+name+`</h5>
-                    <p class="card-text">`+type+`</p>
-                    <p class="card-text">`+breed+`</p>
-                    <p class="card-text">`+birthday+`</p>
+                    <img src="`+img+`" class="card-img-top" alt="`+alt+`">
+                    <h3 class="card-title">`+name+`</h3>
+                    <p class="card-text">Type: `+type+`</p>
+                    <p class="card-text">Breed: `+breed+`</p>
+                    <p class="card-text">Birthday: `+birthday+`</p>
                 </div>
             </div>`
         )
